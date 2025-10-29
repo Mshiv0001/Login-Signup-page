39f74a5477920c010a2e7ac09a9b305d15cbc3df
mohit <mshivhare0001@gmail.com>
2025-10-29 13:22:38 +0530

    Removed basename for Netlify deployment fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import Home from "./Home";   // Home component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 import Login from "./Login";
 import Signup from "./signup";
- // Login component
 
 function App() {
   return (
-    <Router basename="/Login-Signup-page"> {/* must match your GitHub repo name */}
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} />   {/* Home route */}
-        <Route path="/login" element={<Login />} /> {/* Login route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
